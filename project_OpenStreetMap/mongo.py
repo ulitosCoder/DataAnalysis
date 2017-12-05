c333b0b52f39afbce5b44b096715919dc7e3f726
ulitosCoder <ulisesaj83@gmail.com>
2017-12-05 13:22:34 -0600

    Added  audit queries

--- project_OpenStreetMap/mongo.py
+++ project_OpenStreetMap/mongo.py
@@ -10,13 +10,14 @@ if __name__ == "__main__":
     client = MongoClient()
     db = client.test
 
-    print( db.nodes.count())
+    print( "Total nodes {0}".format( db.nodes.count()) )
 
-    print( "Number of nodes: {0}", db.nodes.find({"type":"node"}).count() )
+    print( "Number of nodes: {0}".format( 
+        db.nodes.find({"type":"node"}).count() ))
 
-    print( "Number of ways: {0}", db.nodes.find({"type":"way"}).count() )
+    print( "Number of ways: {0}".format(
+        db.nodes.find({"type":"way"}).count() ))
 
-#"Number of unique users: %d",
-    pprint( db.nodes.find().distinct("created.user") )
+    print( "Number of unique users: {0}".format(
+        len(db.nodes.find().distinct("created.user")) ))
 
-    #pprint(db.nodes.find_one({"type":"node"})) 
